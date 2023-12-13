01f866d22cbc1974696b81b3fdcb6d5e6132fb0b
Will <wgnann@gmail.com>
2023-12-13 14:15:03 -0300

    Refatora trata.py
    trata.py foi modificado para separar a execução do ghostscript da função que chama a GUI

--- trata.py
+++ trata.py
@@ -19,6 +19,9 @@ def novo_nome(path):
 
 class Tratador:
     def __init__(self):
+        self.gui()
+
+    def gui(self):
         self.window = tkinter.Tk()
         lbl_aviso = tkinter.Label(text="Tratador de PDF para impressão", height=2)
         lbl_aviso.pack()
@@ -28,37 +31,39 @@ class Tratador:
         self.window.mainloop()
 
     def tratar_pdf(self, event):
-        gs = GHOSTSCRIPT
         viewer = PDF_VIEWER
-        base = BASE_DIR
-
         path = askopenfilename(
             filetypes=[("PDF", "*.pdf")],
             initialdir=os.path.expanduser('~')
         )
-
-        if (path):
-            output = novo_nome(path)
-
-            process = subprocess.run([
-                gs, '-dBATCH', '-dNOPAUSE',
-                '-dPDFX',
-                '-sDEVICE=pdfwrite',
-                '-sColorConversionStrategy=Gray',
-                '-sPDFSETTINGS=prepress',
-                '-sOutputFile={output}'.format(output=output),
-                '-I', base,
-                '{base}/PDFX_def.ps'.format(base=base),
-                path
+        output = novo_nome(path)
+        returncode = self.pdfx(path)
+        if (returncode != 0):
+            showerror("Erro", "Erro ao processar arquivo. Tem certeza que é um PDF?")
+        else:
+            subprocess.run([
+                viewer, output
             ])
-            if (process.returncode != 0):
-                showerror("Erro", "Erro ao processar arquivo. Tem certeza que é um PDF?")
-            else:
-                subprocess.run([
-                    viewer, output
-                ])
-
         self.window.destroy()
+
+    # devolve process.returncode
+    def pdfx(self, path):
+        gs = GHOSTSCRIPT
+        base = BASE_DIR
+        output = novo_nome(path)
+
+        process = subprocess.run([
+            gs, '-dBATCH', '-dNOPAUSE',
+            '-dPDFX',
+            '-sDEVICE=pdfwrite',
+            '-sColorConversionStrategy=Gray',
+            '-sPDFSETTINGS=prepress',
+            '-sOutputFile={output}'.format(output=output),
+            '-I', base, '{base}/PDFX_def.ps'.format(base=base),
+            path
+        ])
+
+        return process.returncode
 
 def main():
     Tratador()
