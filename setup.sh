d7df1b28625b106d43897f30d69d7ef5b4894959
Will <wgnann@gmail.com>
2024-09-20 10:49:16 -0300

    corrigindo o setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,14 @@ PDFX=$(dpkg -S PDFX_def.ps | cut -d: -f2)
 if [ "$PDFX" == "" ]
 then
     echo "não encontrei o pacote libgs9-common"
+    exit
 fi
 
 ICC=$(dpkg -S ISOuncoated.icc | cut -d: -f2)
 if [ "$ICC" == "" ]
 then
     echo "não encontrei o pacote icc-profiles"
+    exit
 fi
 
 cp $PDFX $BASE_DIR
